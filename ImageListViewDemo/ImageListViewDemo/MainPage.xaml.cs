05e68d506b29eb8a137c8dfd927936ded8259b34
Elvis Xia (Shang Hai Wei Chuang Ruan Jian) <v-xucxia@microsoft.com>
2017-04-28 17:01:27 +0800

    Add project files.

---
+++ ImageListViewDemo/ImageListViewDemo/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ImageListViewDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            contentList.ItemsSource = InitModel();
+        }
+
+        private List<Model> InitModel()
+        {
+            List<Model> list = new List<Model>();
+            for (int i = 0; i < 20; i++)
+            {
+                list.Add(new Model
+                {
+                    Name = "Picture " + i,
+                    Status = "active"
+                });
+            }
+            return list;
+        }
+    }
+}
---
+++ ImageListViewDemo/ImageListViewDemo/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageListViewDemo
+{
+    public class Model
+    {
+        public String Name { get; set; }
+        public String Status { get; set; }
+    }
+}
